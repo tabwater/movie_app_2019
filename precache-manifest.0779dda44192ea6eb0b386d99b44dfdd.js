1a63d56c285b55fb59a36a644f3566269e0ba5ef
UnTabler <trabolkia@naver.com>
2019-08-23 11:12:44 +0900

    Updates

---
+++ precache-manifest.0779dda44192ea6eb0b386d99b44dfdd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2640f24e6befa14af10871ab1a00c1d2",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "f676f5f0c1beef76a1e8",
+    "url": "/movie_app_2019/static/css/main.c1f0a76a.chunk.css"
+  },
+  {
+    "revision": "3350fcf69a8fdf7d4a1c",
+    "url": "/movie_app_2019/static/js/2.1c4cb735.chunk.js"
+  },
+  {
+    "revision": "f676f5f0c1beef76a1e8",
+    "url": "/movie_app_2019/static/js/main.47bb5f12.chunk.js"
+  },
+  {
+    "revision": "8e34f0f3f9415f236696",
+    "url": "/movie_app_2019/static/js/runtime~main.97c70ab9.js"
+  }
+]);
